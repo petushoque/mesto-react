4b3f923a3c0bd0684d89cf50aeccea40ba1aa7be
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-04-26 12:22:51 +0700

    feat: заменить стейты данных о пользователе (имя, описание, аватар) на получаемые данные от CurrentUserContext

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import PopupWithForm from './PopupWithForm'
 import ImagePopup from './ImagePopup'
 
 import api from '../utils/api'
+import CurrentUserContext from './CurrentUserContext'
 
 function App() {
 
@@ -17,9 +18,7 @@ function App() {
   useEffect(() => {
     api.getUserInfo()
     .then((result) => {
-      console.log(currentUser)
-      console.log(result)
-        setCurrentUser(result._id)
+        setCurrentUser(result)
     })
     .catch((err) => {
         console.log(err);
@@ -56,34 +55,36 @@ function App() {
   }
 
   return (
-    <div className="page">    
-      <Header />
-      <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}/>
-      <Footer />
+    <div className="page">
+      <CurrentUserContext.Provider value={currentUser}>    
+        <Header />
+        <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}/>
+        <Footer />
 
-      <PopupWithForm name='add-post' title='Новое место' isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
-        <input className="popup__input popup__input_textarea_signature" id="input-signature" name="signature" type="text" placeholder="Название" required minLength="2" maxLength="30" />
-        <span className="popup__input-error input-signature-error"></span>
-        <input className="popup__input popup__input_textarea_picture" id="input-picture" name="picture" type="url" placeholder="Ссылка на картинку" required />
-        <span className="popup__input-error input-picture-error"></span>
-      </PopupWithForm>
+        <PopupWithForm name='add-post' title='Новое место' isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+          <input className="popup__input popup__input_textarea_signature" id="input-signature" name="signature" type="text" placeholder="Название" required minLength="2" maxLength="30" />
+          <span className="popup__input-error input-signature-error"></span>
+          <input className="popup__input popup__input_textarea_picture" id="input-picture" name="picture" type="url" placeholder="Ссылка на картинку" required />
+          <span className="popup__input-error input-picture-error"></span>
+        </PopupWithForm>
 
-      <PopupWithForm name='edit-avatar' title='Обновить аватар' isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
-        <input className="popup__input popup__input_textarea_avatar" id="input-avatar" name="avatar" type="url" placeholder="Ссылка на картинку" required />
-        <span className="popup__input-error input-avatar-error"></span>
-      </PopupWithForm>
+        <PopupWithForm name='edit-avatar' title='Обновить аватар' isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+          <input className="popup__input popup__input_textarea_avatar" id="input-avatar" name="avatar" type="url" placeholder="Ссылка на картинку" required />
+          <span className="popup__input-error input-avatar-error"></span>
+        </PopupWithForm>
 
-      <PopupWithForm name='edit-profile' title='Редактировать профиль' isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-      <input className="popup__input popup__input_textarea_name" id="input-name" name="username" type="text" placeholder="Введите имя" required minLength="2" maxLength="40" />
-        <span className="popup__input-error input-name-error"></span>
-        <input className="popup__input popup__input_textarea_status" id="input-status" name="status" type="text" placeholder="Введите статус" required minLength="2" maxLength="200" />
-        <span className="popup__input-error input-status-error"></span>
-      </PopupWithForm> 
+        <PopupWithForm name='edit-profile' title='Редактировать профиль' isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+          <input className="popup__input popup__input_textarea_name" id="input-name" name="username" type="text" placeholder="Введите имя" required minLength="2" maxLength="40" />
+          <span className="popup__input-error input-name-error"></span>
+          <input className="popup__input popup__input_textarea_status" id="input-status" name="status" type="text" placeholder="Введите статус" required minLength="2" maxLength="200" />
+          <span className="popup__input-error input-status-error"></span>
+        </PopupWithForm> 
 
-      <PopupWithForm name='delete-post' title='Вы уверены?' isOpen={isDeletePostPopupOpen} onClose={closeAllPopups}>
-      </PopupWithForm> 
+        <PopupWithForm name='delete-post' title='Вы уверены?' isOpen={isDeletePostPopupOpen} onClose={closeAllPopups}>
+        </PopupWithForm> 
              
-      <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+        <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+      </CurrentUserContext.Provider>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,16 @@
-import { React, useState, useEffect } from 'react'
+import { React, useState, useEffect, useContext } from 'react'
 import api from '../utils/api'
 import Card from './Card'
+import CurrentUserContext from './CurrentUserContext';
 
 function Main (props) {
 
-    const [userName, setUserName] = useState('');
-    const [userDescription, setUserDescription] = useState('');
-    const [userAvatar, setUserAvatar] = useState('');
+    const user = useContext(CurrentUserContext)
+
     const [cards, setCards] = useState([])
 
     useEffect(() => {
-        api.getUserInfo()
-        .then((result) => {
-            setUserName(result.name);
-            setUserDescription(result.about);
-            setUserAvatar(result.avatar);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    
+        
         api.getCards()
         .then((result) => {
             const data = result.map((item) => 
@@ -41,14 +32,13 @@ function Main (props) {
 
     return (
         <main className="main">
-    
             <section className="profile">
-                <div className="profile__avatar" onClick={props.onEditAvatar} style={{ backgroundImage: `url(${userAvatar})` }} ></div>  
+                <div className="profile__avatar" onClick={props.onEditAvatar} style={{ backgroundImage: `url(${user.avatar})` }} ></div>  
                 <div className="profile__user">      
-                    <h1 className="profile__name">{userName}</h1>
+                    <h1 className="profile__name">{user.name}</h1>
                     <button className="profile__edit-button" onClick={props.onEditProfile} type="button"></button>
                 </div>
-                <p className="profile__status">{userDescription}</p>
+                <p className="profile__status">{user.about}</p>
                 <button className="profile__add-button" onClick={props.onAddPlace} type="button"></button>
             </section>
 
