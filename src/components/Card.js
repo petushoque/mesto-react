ad6bdd7482218d9c57864656712ef5c36195f9fb
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-06-05 13:58:06 +0700

    feat: отображение активной и неактивной кнопки лайка

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,12 +6,10 @@ function Card (props) {
   const user = useContext(CurrentUserContext)
 
   const isOwn = (props.owner === user._id);
-  const cardDeleteButtonClassName = (`card__delete ${isOwn ? 'card__delete-button_visible' : 'card__delete-button_hidden'}`);
+  const cardDeleteButtonClassName = (`${isOwn ? 'card__delete' : 'card__delete_hidden'}`);
 
   const isLiked = props.likes.some(i => i._id === user._id);
-
-  //будущая переменная, для отображения лайкнутой и не лайкнутой карточки
-  //const cardLikeButtonClassName = `...`; 
+  const cardLikeButtonClassName = `card__like ${isLiked ? 'card__like_active' : ''}`; 
 
   function handleClick() {
     props.onCardClick(props);
@@ -27,11 +25,11 @@ function Card (props) {
           <div className="card__info">
             <h2 className="card__signature">{props.name}</h2>
             <div className="card__like-section">
-              <button className="card__like" type="button" onClick={handleLikeClick}></button>
+              <button className={cardLikeButtonClassName} type="button" onClick={handleLikeClick}></button>
               <p className="card__like-counter">{props.likes.length}</p>
             </div>
           </div>
-          <button className="card__delete" type="button">
+          <button className={cardDeleteButtonClassName} type="button">
           </button>
         </article>
     )
